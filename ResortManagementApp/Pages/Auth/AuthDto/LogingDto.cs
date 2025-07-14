bcaef7bed5b294c7d512be211654f924473a7647
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-07-15 02:55:41 +0530

    update login and registation UI

---
+++ ResortManagementApp/Pages/Auth/AuthDto/LogingDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ResortManagementApp.Pages.Auth.AuthDto
+{
+    public class LogingDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
