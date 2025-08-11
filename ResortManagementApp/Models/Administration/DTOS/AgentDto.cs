9cddea07d90d28c5dfd7e632eb6367d823c65faf
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-08-11 13:29:38 +0530

    push manageAgent Dtos, models, UI

---
+++ ResortManagementApp/Models/Administration/DTOS/AgentDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResortManagementApp.Models.Administration.DTOS
+{
+    public class AgentDto
+    {
+       
+        public Guid Id { get; set; }
+        
+        public string AgentName { get; set; }
+        
+        public string AgentAddress { get; set; }
+
+        public string? ContactPerson { get; set; }
+       
+        public string Mobile { get; set; }
+        public string Fax { get; set; }
+
+        
+        public string Email { get; set; }
+        public string WebSite { get; set; }
+        public double CreditLimit { get; set; }
+        public string VatRegNo { get; set; }
+    }
+}
---
+++ ResortManagementApp/Models/Administration/ManageAgents/Agents.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResortManagementApp.Models.Administration.ManageAgents
+{
+    public class Agents
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string AgentName { get; set; }
+        [Required]
+        public string AgentAddress { get; set; }
+        
+        public string? ContactPerson { get; set; }
+        [Required]
+        [Phone]
+        public string Mobile{ get; set; }
+        public string Fax{ get; set; }
+        
+        [EmailAddress]
+        public string Email{ get; set; }
+        public string WebSite{ get; set; }
+        public double CreditLimit { get; set; }
+        public string VatRegNo { get; set; }
+    }
+}
