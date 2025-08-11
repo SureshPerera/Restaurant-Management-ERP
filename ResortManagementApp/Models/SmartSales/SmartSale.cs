dc418f4ef0fa87920cc56a47c63fe3bbb0998707
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-08-11 16:32:19 +0530

    push SmartSale& billing UI

---
+++ ResortManagementApp/Models/SmartSales/SmartSale.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResortManagementApp.Models.SmartSales
+{
+    public class SmartSale
+    {
+        [Key]
+        public Guid Id { get; set; }
+        
+        public string SandryItem { get; set; }
+        public string UnitPrice { get; set; }
+        
+        
+    }
+}
