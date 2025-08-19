73c45bf46bb5923d1d6c2cb8cbcb80c691f057ae
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-08-20 00:27:16 +0530

    push ClientManagement UI

---
+++ ResortManagementApp/Models/ClientManagement/Message/Message.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResortManagementApp.Models.ClientManagement.Message
+{
+    public class Message
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Type{ get; set; }
+        [Required]
+        public string Messages{ get; set; }
+
+    }
+}
---
+++ ResortManagementApp/Models/ClientManagement/Message/MessageDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResortManagementApp.Models.ClientManagement.Message
+{
+    public class MessageDto
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Messages { get; set; }
+    }
+}
