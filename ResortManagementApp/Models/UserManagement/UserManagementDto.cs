901a998c6a56bf19e0d1d6d1d638f287a5d09205
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-08-20 14:28:23 +0530

    push user Manegement DTO model and UI

---
+++ ResortManagementApp/Models/UserManagement/UserManagementDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResortManagementApp.Models.UserManagement
+{
+    public class UserManagementDto
+    {
+        public Guid Id { get; set; }
+        public string NIC { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PassWord { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+   
+        public string AccessLevel { get; set; }
+        public DateOnly DOB { get; set; }
+    
+        public string Gender { get; set; }
+        public string? Address { get; set; }
+        public string? Comment { get; set; }
+        public bool? DashBoard { get; set; }
+        public bool? Reservation { get; set; }
+        public bool? CheckInOut { get; set; }
+        public bool? Inhouse { get; set; }
+        public bool? HouseKeeping { get; set; }
+        public bool? SmartSalling { get; set; }
+        public bool? StockInventory { get; set; }
+        public bool? AssetsManagement { get; set; }
+        public bool? Administration { get; set; }
+        public bool? UserManagements { get; set; }
+        public bool? ClientManagement { get; set; }
+    }
+}
